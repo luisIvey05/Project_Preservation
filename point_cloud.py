82d59a5bd1e843274e796f23495e41fe7d0fb7e5
luisivey05 <livey@bu.edu>
2022-10-18 22:11:26 -0400

    [FIX] Temporary fix until glibc is install on the scc

--- point_cloud.py
+++ point_cloud.py
@@ -1,7 +1,9 @@
 import open3d as o3d
+import numpy as np
+from PIL import Image
 
-
-def get_3d_point_cloud(image_pth, depth):
+def get_3d_point_cloud(image_pth, depth_pth):
+    depth = np.array(Image.open(depth_pth)).astype(np.float32)
     rgbd = o3d.geometry.RGBDImage.create_from_color_and_depth(o3d.io.read_image(image_pth), o3d.geometry.Image(depth),
                                                               convert_rgb_to_intensity=False)
     intrinsic = o3d.camera.PinholeCameraIntrinsic(width = 1242, height = 375, fx = 721., fy = 721., cx = 609., cy = 609.)
